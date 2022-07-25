37221f483dfbfcbf3751c8a5de14034e7f022c6e
Pranav Pradeep <prnvprdp@gmail.com>
2022-07-25 17:41:34 +0530

    fixed bugs and general imporvement

---
+++ __main__.py
@@ -0,0 +1,4 @@
+import reboot_router
+
+if __name__ == '__main__':
+    reboot_router.main()
--- reboot_router.py
+++ reboot_router.py
@@ -11,21 +11,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 This scripts reboots your router using selenium web driver. 
 This script was written for tplink archer c5 router
 """
-config = ConfigParser()
-config.read('pass.ini' if os.path.exists('pass.ini') else 'pass_git.ini')
+file_path = os.path.dirname(os.path.realpath(__file__))
 
+config = ConfigParser()
+config_file_path = os.path.join(file_path, 'pass.ini') if os.path.exists(
+    os.path.join(file_path, 'pass.ini')) else os.path.join(file_path, 'pass_git.ini')
+config.read(config_file_path)
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(config.get('SETTINGS', 'chrome_driver'), options=chrome_options)
-driver.get(config.get('SETTINGS', 'router_url'))
-driver.find_element('id', 'pc-login-password').send_keys(config.get('SETTINGS', 'password'))
-driver.find_element('id', 'pc-login-btn').click()
-try:
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'topReboot')))
-    element.click()
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, "//span[.='Yes']")))
-    element.click()
-finally:
-    driver.quit()
+
+def main():
+    driver = webdriver.Chrome(config.get('SETTINGS', 'chrome_driver'), options=chrome_options)
+    driver.get(config.get('SETTINGS', 'router_url'))
+    driver.find_element('id', 'pc-login-password').send_keys(config.get('SETTINGS', 'password'))
+    driver.find_element('id', 'pc-login-btn').click()
+    try:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'topReboot')))
+        element.click()
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, "//span[.='Yes']")))
+        element.click()
+    finally:
+        driver.quit()
